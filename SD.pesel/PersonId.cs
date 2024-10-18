4b1be74432636c3164de67941a73acab59d994d1
Rdzanek Mateusz <rdzanek.mateusz@SZKOLA.ZST-RADOM.EDU.PL>
2024-10-18 15:42:53 +0200

    mati

--- SD.pesel/PersonId.cs
+++ SD.pesel/PersonId.cs
@@ -1,64 +1,89 @@
-﻿namespace SD.pesel;
+﻿using System;
 
-public class PersonId
+namespace SD.pesel
 {
-    private readonly string _id;
+    public class PersonId
+    {
+        private readonly string _id;
 
-    public PersonId(string Id)
-    {
-        _id = Id;
-    }
-    /// <summary>
-    /// Get full year from PESEL
-    /// </summary>
-    /// <returns></returns>
-    public int GetYear()
-    {
-        return 0;
-    }
+        public PersonId(string id)
+        {
+            if (string.IsNullOrEmpty(id) || id.Length != 11)
+            {
+                throw new ArgumentException("PESEL musi mieć 11 znaków.");
+            }
+            _id = id;
+        }
 
-    /// <summary>
-    /// Get month from PESEL
-    /// </summary>
-    public int GetMonth()
-    {
-        return 0;
-    }
+        public int GetYear()
+        {
+            int year = int.Parse(_id.Substring(0, 2)); 
+            int month = int.Parse(_id.Substring(2, 2)); 
 
-    /// <summary>
-    /// Get day from PESEL
-    /// </summary>
-    /// <returns></returns>
-    public int GetDay()
-    {
-        return 0;
-    }
 
-    /// <summary>
-    /// Get year of birth from PESEL
-    /// </summary>
-    /// <returns></returns>
-    public int GetYearOfBirth()
-    {
-        return 0;
-    }
+            if (month > 20)
+            {
+                year += 2000;
+            }
+            else
+            {
+                year += 1900;
+            }
 
-    /// <summary>
-    /// Get gender from PESEL
-    /// </summary>
-    /// <returns>m</returns>
-    /// <returns>f</returns>
-    public string GetGender()
-    {
-        return "";
-    }
+            return year;
+        }
 
-    /// <summary>
-    /// check if PESEL is valid
-    /// </summary>
-    /// <returns></returns>
-    public bool IsValid()
-    {
-        return true;
+ 
+        public int GetMonth()
+        {
+            int month = int.Parse(_id.Substring(2, 2)); 
+
+         
+            if (month > 20)
+            {
+                month -= 20;
+            }
+
+            return month;
+        }
+
+
+        public int GetDay()
+        {
+            return int.Parse(_id.Substring(4, 2)); 
+        }
+
+        public int GetYearOfBirth()
+        {
+            return GetYear();
+        }
+
+
+        public string GetGender()
+        {
+            int genderDigit = int.Parse(_id.Substring(9, 1));
+            return genderDigit % 2 == 0 ? "f" : "m";
+        }
+        public bool IsValid()
+        {
+            
+            if (_id.Length != 11 || !long.TryParse(_id, out _))
+            {
+                return false;
+            }
+
+            int[] weight = new int[] { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+            int sum = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                sum += (int.Parse(_id[i].ToString()) * weight[i]);
+            }
+
+            int controlDigit = (10 - (sum % 10)) % 10;
+            int lastDigit = int.Parse(_id[10].ToString());
+
+            return controlDigit == lastDigit;
+        }
     }
 }
